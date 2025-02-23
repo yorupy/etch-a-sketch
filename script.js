fae527f4848790c94e555bd1255a42f0a23bd65a
yorupy <198967142+yorupy@users.noreply.github.com>
2025-02-22 21:50:55 -0500

    Allow grid resizing

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ function createCell(size) {
     return cell;
 }
 
+function cleanGrid() {
+    const grid = document.querySelector(".grid");
+    grid.replaceChildren();
+}
+
 function fillGrid(size = 16) {
     const grid = document.querySelector(".grid");
     for (let i = 0; i < size * size; i++) {
@@ -21,4 +26,25 @@ function handleCellHover(event) {
     event.target.style.backgroundColor = "black"
 }
 
+function handleResize() {
+    const resizeButton = document.querySelector(".resize");
+    resizeButton.addEventListener('click', () => {
+        let newSize = "";
+        do {
+            newSize = prompt("Input a new dimension for the grid (min 16, max 100): ");
+        } while (!validateSize(newSize));
+        cleanGrid();
+        fillGrid(newSize);
+    });
+}
+
+function validateSize(size) {
+    if (isNaN(size)) {
+        return false;
+    }
+    const sizeNumber = Number(size);
+    return sizeNumber >= 16 && sizeNumber <= 100;
+}
+
 fillGrid();
+handleResize();
